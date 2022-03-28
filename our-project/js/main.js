ec925a1f685f3a65d698a6b1b196a235e8fd4880
Laila317 <lailam.ibrahim317@gmail.com>
2022-03-28 10:08:40 -0400

    l

--- our-project/js/main.js
+++ our-project/js/main.js
@@ -1,1 +1,9 @@
+import { countries} from "../js/maybe";
 import "../styles/style.css";
+
+
+const DOMSelectors = {
+    Cold: document.getElementById("b1"),
+    All:document.getElementById("b2"),
+    Hot: document.getElementById("b3"),
+  };
---
+++ our-project/js/maybe.js
@@ -0,0 +1,36 @@
+
+const countries=[
+
+{
+Name: "Norway", 
+Cold: true
+},
+
+{
+Name: "Denmark",
+Cold: true
+},
+
+{
+Name: "Canada",
+Cold: true   
+},
+
+{
+Name: "South Africa",
+Cold: false
+},
+
+{
+Name: "Egypt",
+Cold: false
+},
+
+{
+Name: "Mexico",
+Cold: false
+}
+
+];
+
+export{countries};
