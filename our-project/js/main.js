c7057796ea5310ea04ca0f6f10d931760ffe65df
n <nling1026@gmail.com>
2022-04-25 09:46:00 -0400

    c

--- our-project/js/main.js
+++ our-project/js/main.js
@@ -1,2 +1,6 @@
 import "../styles/style.css";
-/* import { gsap } from "gsap"; */
+import { gsap } from "gsap";
+
+gsap.to("h2.title", { duration: 1, opacity: 0.3 });
+gsap.to(".box", { duration: 2, x: 300 });
+gsap.to(".green", { duration: 3, rotation: 360, scale: 0.5 });
