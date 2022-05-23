cc867fccaa4feb6bf06ce0a7d3e3f2a4f6391ed7
Laila317 <lailam.ibrahim317@gmail.com>
2022-05-23 19:53:55 -0400

    jjj

--- our-project/js/main.js
+++ our-project/js/main.js
@@ -1,3 +1,7 @@
 import "../styles/style.css";
 import { gsap } from "gsap";
+import{ScrollTrigger} from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
+const tl= gsap.timeline({delay:0.2});
+tl.from(".col-1", {opacity:0, duration:2.4})
