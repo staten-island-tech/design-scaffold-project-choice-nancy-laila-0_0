f7452bfb08c167d596dafc56582c6988b7c52685
Laila317 <lailam.ibrahim317@gmail.com>
2022-05-24 21:32:29 -0400

    menu almost done?? still need media queries

--- our-project/js/main.js
+++ our-project/js/main.js
@@ -3,5 +3,60 @@ import { gsap } from "gsap";
 import{ScrollTrigger} from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-const tl= gsap.timeline({delay:0.2});
-tl.from(".col-1", {opacity:0, duration:2.4})
+
+const menuList=document.getElementById("menuList")
+menuList.style.maxHeight="0px"
+
+function togglemenu(){
+    if(menuList.style.maxHeight=="0px")
+    {
+        menuList.style.maxHeight="130px"
+    }
+    else{
+        menuList.style.maxHeight="0px"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const tl1= gsap.timeline({ScrollTrigger: ".col-2", delay:0.2});
+tl1.from(".col-1", {opacity:0, duration:2.4})
+
+const tl2=gsap.timeline({scrollTrigger:".T1", delay:0.2})
+tl2.from(".T1", { duration: 0.2, opacity: 0,   scale: 0.8, translateY: 50, transformOrigin: 'center center', })
+
+const tl3=gsap.timeline({scrollTrigger:".T2", delay:0.2})
+tl3.from(".T2", { duration: 0.2, opacity: 0,   scale: 0.8, translateY: 50, transformOrigin: 'center center', })
+
+const tl4=gsap.timeline({scrollTrigger:".T3", delay:0.2})
+tl4.from(".T3", { duration: 0.2, opacity: 0,   scale: 0.8, translateY: 50, transformOrigin: 'center center', })
+
+const tl5=gsap.timeline({scrollTrigger:".T4", delay:0.2})
+tl5.from(".T4", { duration: 0.2, opacity: 0,   scale: 0.8, translateY: 50, transformOrigin: 'center center', })
+
+
+
+
+
+
+
+
+
+
+/*tl.to(".T1", { duration: 0.2, opacity: 0,   scale: 0.8, translateY: 50, transformOrigin: 'center center', })*/
